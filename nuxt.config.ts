c08777a26da6b09ab197a8b07945e027498c47a2
munkh-altai <munkaltai@gmail.com>
2022-11-10 14:36:04 +0800

    nuxt latest version and nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,31 +46,34 @@ export default defineNuxtConfig({
       },
     },
   },
-  build: {
-    postcss: {
-      postcssOptions: require("./postcss.config.js"),
-    },
+  postcss: {
+    config:true,
+    plugins:{
+      tailwindcss: {},
+      autoprefixer: {},
+    }
   },
 
-  meta: {
-    meta: [
-      { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
-      { name: "charset", content: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1" },
-      { name: "description", content: process.env.LAMBDA_DESCRIPTION  },
-      { name: "og:url", content: process.env.LAMBDA_FAVICON },
-      { name: "og:type", content: "article" },
-      { name: "og:title", content: process.env.LAMBDA_TITLE },
-      { name: "og:description", content: process.env.LAMBDA_DESCRIPTION },
-      { name: "og:image", content: process.env.LAMBDA_FAVICON},
-    ],
-    link: [
-      { rel: "icon", href: process.env.LAMBDA_FAVICON },
-    ],
+  app:{
+    head:{
+      meta: [
+        { name: "charset", content: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1" },
+        { name: "description", content: process.env.LAMBDA_DESCRIPTION  },
+        { name: "og:url", content: process.env.LAMBDA_FAVICON },
+        { name: "og:type", content: "article" },
+        { name: "og:title", content: process.env.LAMBDA_TITLE },
+        { name: "og:description", content: process.env.LAMBDA_DESCRIPTION },
+        { name: "og:image", content: process.env.LAMBDA_FAVICON},
+      ],
+      link: [
+        { rel: "icon", href: process.env.LAMBDA_FAVICON },
+      ],
+    }
   },
+
   generate: {
     routes: ["/"],
-    subFolders: true,
   },
   ssr: false,
 });
